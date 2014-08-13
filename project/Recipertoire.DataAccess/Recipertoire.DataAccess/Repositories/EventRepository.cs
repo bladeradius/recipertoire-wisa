7bcae95d68af5ee9642c6e2715e98ed4b3613559
Timothy Simmons <timosimmons@gmail.com>
2014-08-13 15:24:14 -0500

    Data access layer and Events table created

---
+++ project/Recipertoire.DataAccess/Recipertoire.DataAccess/Repositories/EventRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Recipertoire.DataAccess.Models;
+
+namespace Recipertoire.DataAccess.Repositories
+{
+    class EventRepository : IEventRepository
+    {
+        private RecipertoireEntities m_Entities = new RecipertoireEntities();
+
+        public IEnumerable<Event> List()
+        {
+            return m_Entities.Events.ToList();
+        }
+
+        public bool Create(Event newEvent)
+        {
+            try
+            {
+                m_Entities.Events.Add(newEvent);
+                m_Entities.SaveChanges();
+                return true;
+            }
+            catch 
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(Event existingEvent)
+        {
+            try
+            {
+                m_Entities.Events.Remove(existingEvent);
+                m_Entities.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Edit(Event existingEvent)
+        {
+            try
+            {
+                m_Entities.Entry(existingEvent).State = System.Data.Entity.EntityState.Modified;
+                m_Entities.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ project/Recipertoire.DataAccess/Recipertoire.DataAccess/Repositories/IEventRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Recipertoire.DataAccess.Models;
+
+namespace Recipertoire.DataAccess.Repositories
+{
+    interface IEventRepository
+    {
+        IEnumerable<Event> List();
+        bool Create(Event newEvent);
+        bool Delete(Event existingEvent);
+        bool Edit(Event existingEvent);
+    }
+}
